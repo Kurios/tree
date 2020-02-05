806a7f40c4b4325caf969c21ec919803b752b223
Jack Morgan <jmorgan@aravo.com>
2020-02-05 15:55:52 -0800

    initial tree

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+char img[] = "\
+                                                                                \n\
+                               `:                                               \n\
+                          --:`:so:`   .:          `.                            \n\
+                        . -oys:yhhh:/oyho`.`/.`.`-:-     ``                     \n\
+                        /:/oyhyhhhys+oy+.//+os::yoo: `:::+/                     \n\
+                   ```  `./ysyhhhyyyoohoos+yhys+++ys/:+yy/-`   `.--             \n\
+                   --/:+/+++syooo/+ss+sddddmdmddhhhsyyhmmhhyo++ooy+             \n\
+         ` ``-.`/. .:/+ssosyy/os+///ossmmdNmmdhhdNmdNNNNmmNmmmmmmdds`           \n\
+        -ooosyh/shoo+///+/+yhy+sssoyyhhhhhyyddmmmmNmmNNdhdmmmNmNNNNho+`         \n\
+       `+hhyyydhhhdh/:+so+++s/yyhhhhdhdmsyhdhNmNNNNmdydhmhhmmdmmmNNNmdo-.`      \n\
+      .oydyydmNNmddyohyoo/+/ooyddddNNhydhhyyhysdmdsshddyhddmmddmmmNNmmdhyyo.    \n\
+     `-/+ssyhmmmhsossos+/oyyyhhsdNNhyhhmhydmhyysyydddhdmmmdNNmhymNmyNNNmmh+..`  \n\
+     -ss+hhhyhydh/soyss+syhhmddhdNmmmdmddddmmddddmmsmNNhmmd+hmNNhhydmmNNmy+o/-  \n\
+  .:/+soyhhdydooo/sshsoshhmhhddddmhmsodhdmhdNNNNNddo/ymhNys/+hmNdh++ohmdmy++.   \n\
+  `oyshydmyhydyyhhyhddyshymmdNmmmNdhdhmhhdddNmmmmdmhyyddhshmhdmmNNmmmmysmNhy+   \n\
+   .+:sy+ohNmmooyhhhhhdhsoyssdNNmNhymmdmmmdNmmNmhyyhhhdhhhdydhmmdsyhdddhmdms:   \n\
+      `+oshhoshooddyydNyoyyyhmmmNmyhdmyyhyddhhmdyydmhysoyhhho-sos:-ydmmsmmy--`  \n\
+      `:+:oyso-+osoohdmhshsdhmmddhsyhhyhhsyyhhhhyhdmyyhhhhs:`-.-hhhhdNmmmdo-s-  \n\
+         ::y+s+os/oyosys/syhdhhshmdmmyysmdmNdddhyhdmyhddds::ymddmdsyyyhdyyo/`   \n\
+         .+yoo/ooyyss+ossdmyhsyyhddhdsohdmNmmmdhyhsNhdmmmd+yddmNmh/:` ````      \n\
+    `....-+o+soyddmmyshdddmsyhmdyydhymdhmmmdNNmhysyhsohmymsyhydmyhd/`           \n\
+    ```:os++oddhhdhddmmmmmmdmho/sdmmmNNNmNhhmdmdmmmhdddmdmNymmmh:`/h.           \n\
+    `::/sy++ooyydmhydmNmdmmmmys:/hNmdNNmmNdhdyhmNmNNmmmmNmdsmdhd+  `            \n\
+    -::/+oo+oyhddmNNmmmyhhsyhmdmddNddMy+mmNdmmNNNmmdmNNmmmNNNNmdhs:`            \n\
+   `-...-./sdhyshdy+:y+//.-/ydNmm+hdsNosdNNNNNmNyoo+ysNNNNNNmNNmdmdo`           \n\
+          .:+oosy+-. ` `../shdNNd+dNsNhdNNmmNmNmys:.`-yydNNNNNmhNmhs.           \n\
+            .-`--..`      `o+yddmmNMdNMmoso/dmdyyydy+ydddmmyddhhmh.             \n\
+                         `:os+oydhmNNMhd/--+doh/ys/:sy:--/yhs+ymh/              \n\
+                          `.- -:osyNNNyydshs../syo/  ``   ./:  -/:              \n\
+                                /o+dNmmmy/`    -o.                              \n\
+                                -:`hNNm.                                        \n\
+                                   hNMo                                         \n\
+                                  `dNM+                                         \n\
+                                  .dNNo                                         \n\
+                                  .dmm+                                         \n\
+                                  :hdm+                                         \n\
+                                  +hddy                                         \n\
+                               `..+oso++:-.`                                    \n\
+                                  ``                                            \n\
+                                                      Tree (c)Microsoft 1999    \n\
+";
+
+int main() {
+    printf(img);
+}
